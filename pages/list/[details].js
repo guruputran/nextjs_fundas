843632722c1097c5e329c0b2af18ba3349f3d701
guruputran <gpsmartcodes@gmail.com>
2023-05-28 13:17:41 +0800

    funda 2

--- pages/list/[details].js
+++ pages/list/[details].js
@@ -2,12 +2,23 @@
 
 import React from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 
-export default function ListDetails() {
-  const router = useRouter();
-  const { query } = router;
-  const { details } = query;
+const the__list = [
+  {
+    id: 1,
+    name: "List 1",
+  },
+  {
+    id: 2,
+    name: "List 2",
+  },
+  {
+    id: 3,
+    name: "List 3",
+  },
+];
+export default function ListDetails(props) {
+  const { id, name } = props;
   return (
     <div>
       <Link href="/">
@@ -18,17 +29,36 @@ export default function ListDetails() {
           Home Page
         </button>
       </Link>
-      <Link href="/list">
-        <button
-          type="button"
-          className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-        >
-          List
-        </button>
-      </Link>
-      <h2 className="text-3xl text-pink-600 font-bold">
-        List details for {details}
+      <h2>
+        the id is : {id} and the name is: {name}
       </h2>
     </div>
   );
+}
+
+export async function getStaticPaths() {
+  //create all paths here
+  const allListItemPaths = the__list.map((item) => ({
+    params: {
+      details: item.id.toString(),
+    },
+  }));
+
+  return {
+    paths: allListItemPaths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const { details } = params;
+  //filter for detailed view from list
+  const filteredListItem = the__list.find(
+    (item) => item.id.toString() === details
+  );
+  console.log(filteredListItem);
+  return {
+    props: filteredListItem,
+  };
 }
